d41d78e16233ba1209c6c4e5ca629cb5a498a20b
Nikola Bogicevic <nbogicevic2@gmail.com>
2023-11-07 19:36:09 -0500

    dropping

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -105,9 +105,14 @@ class ProjectItem {
 	}
 
 	connectDrag() {
-		document.getElementById(this.id).addEventListener("dragstart", (event) => {
+		const item = document.getElementById(this.id);
+		item.addEventListener("dragstart", (event) => {
 			event.dataTransfer.setData("text/plain", this.id);
 			event.dataTransfer.effectAllowed = "move";
+		});
+
+		item.addEventListener("dragend", (event) => {
+			console.log(event);
 		});
 	}
 
@@ -165,6 +170,29 @@ class ProjectList {
 			if (event.dataTransfer.types[0] === "text/plain") {
 				event.preventDefault();
 			}
+		});
+
+		list.addEventListener("dragleave", (event) => {
+			if (
+				event.relatedTarget.closest &&
+				event.relatedTarget.closest(`#${this.type}-projects ul`) !== list
+			) {
+				list.parentElement.classList.remove("droppable");
+			}
+		});
+
+		list.addEventListener("drop", (event) => {
+			event.preventDefault();
+			const prjId = event.dataTransfer.getData("text/plain");
+			if (this.projects.find((p) => p.id === prjId)) {
+				return;
+			}
+			document
+				.getElementById(prjId)
+				.querySelector("button:last-of-type")
+				.click();
+			list.parentElement.classList.remove("droppable");
+			// event.preventDefault(); // not required
 		});
 	}
 
